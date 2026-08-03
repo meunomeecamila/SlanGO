65ceb49467a6daf5b3b585cbd7a475545956d640
Vitor Chagas <vitorcjagas@gmail.com>
2026-08-03 16:39:31 -0300

    Index padrao

--- slangoServer/src/index.ts
+++ slangoServer/src/index.ts
@@ -3,11 +3,6 @@ import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`🚀 Servidor rodando na porta ${PORT}`);
-    });
-}
-
-export default app;
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando na porta ${PORT}`);
+});
