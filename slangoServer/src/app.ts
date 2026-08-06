e32eeb07c4597e70f21fcc6eeff65db378e7a4b4
Vitor Chagas <vitorcjagas@gmail.com>
2026-08-06 15:23:43 -0300

    Add rota

--- slangoServer/src/app.ts
+++ slangoServer/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mundoRoutes from './routes/mundoRoutes';
 import usuarioRoutes from './routes/usuarioRoutes';
+import recuperarRoutes from './routes/recuperarRoutes';
 
 const app = express();
 
@@ -9,5 +10,5 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', usuarioRoutes);
 app.use('/api', mundoRoutes);
-
+app.use('/api', recuperarRoutes);
 export default app;
